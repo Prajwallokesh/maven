d569c425b679516c25e502bb2d0581b502c6d182
Prajwallokesh <159251401+Prajwallokesh@users.noreply.github.com>
2026-05-17 23:13:02 +0530

    first commit

---
+++ src/main/java/devops/prgm2/App.java
@@ -0,0 +1,13 @@
+package devops.prgm2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
